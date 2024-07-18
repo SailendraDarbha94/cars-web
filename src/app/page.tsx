02ebbe82ef82647740bfb5fcdc8d2b180155a4c3
SailendraDarbha94 <sailendra.darbha@gmail.com>
2024-07-18 16:43:08 +0530

    header change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl font-pBold text-black mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
-              Cars
+              CARS
             </h1>
             <p className="max-w-2xl mb-6 font-pMedium font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
               Claims Adjustment & Reporting System
